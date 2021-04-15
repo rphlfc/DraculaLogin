87a4619b3777653222ac221318e2bb5118970d96
Raphael Cerqueira <raphaelfc@gmail.com>
2021-04-15 10:34:53 -0300

    Initial Commit

---
+++ DraculaLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DraculaLogin
+//
+//  Created by Raphael Cerqueira on 15/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DraculaLogin/DraculaLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraculaLoginApp.swift
+//  DraculaLogin
+//
+//  Created by Raphael Cerqueira on 15/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DraculaLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
